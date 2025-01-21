37585b586e39a51ddc70bf64ef97f22bea7f90cf
kiran8884146600 <143705462+kiran8884146600@users.noreply.github.com>
2025-01-21 11:10:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_api_gateway_method" "hello-world-func_method" {
 
 # API Gateway Integration to Lambda
 resource "aws_api_gateway_integration" "lambda_integration" {
-  rest_api_id             = aws_api_gateway_rest_api.example.id
+  rest_api_id             = aws_api_gateway_rest_api.app_api.id
   resource_id             = aws_api_gateway_resource.hello-world-func_resource.id
   http_method             = aws_api_gateway_method.hello-world-func_method.http_method
   integration_http_method = "POST"
