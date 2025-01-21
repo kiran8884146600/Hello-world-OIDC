33747a17011a5392d90e421c3440198ed25d8094
kiran8884146600 <143705462+kiran8884146600@users.noreply.github.com>
2025-01-21 10:44:18 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,11 +38,11 @@ resource "aws_api_gateway_method" "helloworld_method" {
 # Integration
 resource "aws_api_gateway_integration" "lambda_integration" {
   rest_api_id             = aws_api_gateway_rest_api.app_api.id
-  resource_id             = aws_api_gateway_resource.helloworld_resource.id
-  http_method             = aws_api_gateway_method.helloworld_method.http_method
+  resource_id             = aws_api_gateway_resource.hello-world-func_resource.id
+  http_method             = aws_api_gateway_method.hello-world-func_method.http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
-  uri                     = aws_lambda_function.helloworld.invoke_arn
+  uri                     = aws_lambda_function.hello-world-func.invoke_arn
 }
 
 # Deployment
@@ -56,7 +56,7 @@ resource "aws_api_gateway_deployment" "api_deployment" {
 resource "aws_lambda_permission" "api_permission" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.helloworld.arn
+  function_name = aws_lambda_function.hello-world-func.arn
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_api_gateway_rest_api.app_api.execution_arn}/*/*"
 }
