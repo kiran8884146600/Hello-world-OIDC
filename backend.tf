1d88faae5fbde5ddd2e3d9c2686fa4bf4a0f3642
kiran8884146600 <143705462+kiran8884146600@users.noreply.github.com>
2025-01-29 10:54:46 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket         = "kiran271222"
     key            = "myfold/terraform.tfstate"
-    region         = "us-east-1" 
+    region         = "us-east-1"
   }
 }
